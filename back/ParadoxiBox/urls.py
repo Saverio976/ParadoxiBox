7ee623d24cbd18aeaeb067cc68e824a5f02d9691
Xavier Mitault <xavier.mitault@epitech.eu>
2023-08-28 14:07:40 +0200

    Fix import

--- back/ParadoxiBox/urls.py
+++ back/ParadoxiBox/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from django.views.static import serve
+from django.conf import settings
 
 from .api import api
 from .views import redirect_api_docs
