f2a12cb4f98d204de0fcc4f0d2853d75842ea5f5
Xavier Mitault <xavier.mitault@epitech.eu>
2023-08-29 08:12:47 +0200

    Fix: api call next

--- front-cli/src/api_next.v
+++ front-cli/src/api_next.v
@@ -4,7 +4,7 @@ import net.http
 
 fn api_next(api_url string, bearer string) ! {
 	resp := http.fetch(http.FetchConfig{
-		url: api_url + '/songs/next'
+		url: api_url + '/songs/queue/next'
 		method: http.Method.get
 		header: http.new_header_from_map({
 			http.CommonHeader.authorization: 'Bearer ' + bearer
