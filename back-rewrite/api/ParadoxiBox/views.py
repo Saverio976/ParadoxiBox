9e513e2a3cf2317e7153971c1cedee5590a8e649
Xavier Mitault <xavier.mitault@epitech.eu>
2023-08-31 08:09:51 +0200

    Refactor: (back-rewrite) re use the idea of v everywhere (but the
    download will use yt-dlp and the search ytmusicapi)

--- back-rewrite/api/ParadoxiBox/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import redirect
-
-
-def redirect_api_docs(_):
-    response = redirect("/api/docs")
-    return response
--- back-rewrite/api/songs/logger.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-LOG_FILE = ".song.log"
-
-
-def logger_print(*args: Any, **kwargs: Any) -> None:
-    with open(LOG_FILE, mode="a") as f:
-        print(*args, **kwargs, file=f)
